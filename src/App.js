d2b0f3b269d45c7bb15c9ce2c2c34634c4b4374a
久慈和輝 <kazuaki.kuji@g.dreamcareer.co.jp>
2024-05-22 14:06:46 +0900

    アクセストークン更新確認

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import React, { useState } from 'react';
-import PrefectureCheckboxes from "./components/PrefectureCheckboxes";
-import CreateChart from "./components/CreateChart";
-import usePopulationData from './hooks/usePopulationData';
+import React from 'react';
 import RegisterLocation from './components/RegisterLocation';
 
 function App() {
-  const [selectedPrefectures, setSelectedPrefectures] = useState([]);
-
-  const populationData = usePopulationData(selectedPrefectures);
-
-  const handleCheckboxChange = (pref) => {
-    const isExistencePref = selectedPrefectures.findIndex((item) => item.prefCode === pref.prefCode);
-    if (isExistencePref === -1) { // チェックされたとき（新規で都道府県が追加されたとき）
-      setSelectedPrefectures([...selectedPrefectures, pref]);
-    } else {
-      setSelectedPrefectures(selectedPrefectures.filter((item) => item.prefCode !== pref.prefCode));
-    }
-  }
-
   return (
     <div className="body">
       <header></header>
       <main>
-        <RegisterLocation  />
-        {/* <PrefectureCheckboxes onChange={handleCheckboxChange} selectedPrefectures={selectedPrefectures} />
-        <CreateChart populationData={populationData} selectedPrefectures={selectedPrefectures} /> */}
+        <RegisterLocation />
       </main>
       <footer></footer>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/PrefectureCheckboxes.js
+++ src/components/PrefectureCheckboxes.js
@@ -1,46 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { fetchPrefectures } from '../api/prefectures';
+import React from 'react';
 
-/**
- * 都道府県情報をチェックボックス付きで表示し、選択を管理する
- * @param {Object} props - コンポーネントのプロパティ
- * @param {Function} props.onChange - チェックボックス変更時に呼び出されるコールバック関数
- * @param {Array} props.selectedPrefectures - 選択された都道府県のリスト
- *
- * @returns {JSX.Element} - 都道府県チェックボックスを含むJSX要素
- */
-export default function PrefectureCheckboxes({ onChange, selectedPrefectures }) {
-    const [prefData, setPrefData] = useState([]);
-
-    useEffect(() => {
-        const loadPrefectures = async () => {
-            try {
-                const data = await fetchPrefectures();
-                setPrefData(data);
-            } catch (error) {
-                console.error('都道府県データの取得に失敗しました。', error);
-            }
+const PrefectureCheckboxes = ({ onChange, selectedPrefectures }) => {
+    const handleChange = (event) => {
+        const pref = {
+            prefCode: event.target.value,
+            prefName: event.target.name,
         };
-
-        loadPrefectures();
-    }, []);
+        onChange(pref);
+    };
 
     return (
-        <div className="navApp">
-            <ul style={{ display: "flex", flexWrap: "wrap", listStyle: "none", userSelect: "none" }}>
-                {prefData.map((pref, index) => (
-                    <li key={index} className="pref_itemBox">
-                        <label>
-                            <input
-                                type="checkbox"
-                                checked={selectedPrefectures.some((item) => item.prefCode === pref.prefCode)}
-                                onChange={() => onChange(pref)}
-                            />
-                            <span className="pref_text">{pref.prefName}</span>
-                        </label>
-                    </li>
-                ))}
-            </ul>
+        <div>
+            {selectedPrefectures.map((pref, index) => (
+                <label key={index}>
+                    <input
+                        type="checkbox"
+                        value={pref.prefCode}
+                        name={pref.prefName}
+                        checked={selectedPrefectures.some((item) => item.prefCode === pref.prefCode)}
+                        onChange={handleChange}
+                    />
+                    {pref.prefName}
+                </label>
+            ))}
         </div>
     );
-}
+};
+
+export default PrefectureCheckboxes;
