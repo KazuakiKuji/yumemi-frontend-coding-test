7e29a8890eca210e0db17338a658c96aac227f55
久慈和輝 <kazuaki.kuji@g.dreamcareer.co.jp>
2024-05-22 15:04:15 +0900

    Header, Footerコンポーネントの作成 + Header, Contents, Footerそれぞれにcssを作成

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./assets/style/global.css";
 import reportWebVitals from './reportWebVitals';
-
-import TESTApp from "./TESTApp"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <TESTApp />
-
   <React.StrictMode>
     <App />
   </React.StrictMode>
